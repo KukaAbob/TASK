2ba5ca2e08b7d6093e3540c050d07690d5951340
KukaAbob <146316346+KukaAbob@users.noreply.github.com>
2023-11-09 10:18:36 +0300

    Update sql.sql
    1.1v

---
+++ sql.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE ABOB;
+
+CREATE TABLE ABOB32 (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100),
+    age INT,
+    gender VARCHAR(10)
+);
+
+INSERT INTO ABOB32 
+VALUES (1 , abob , 12 , helicop);
