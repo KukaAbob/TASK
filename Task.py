fd2919e2d14f5b6be86211418160f67fc62d8f5b
KukaAbob <146316346+KukaAbob@users.noreply.github.com>
2023-11-09 10:18:15 +0300

    Update Task.py
    1.5v

--- Task.py
+++ Task.py
@@ -1,11 +1,53 @@
-import re
-from robobrowser import RoboBrowser
-print('123')
-br = RoboBrowser()
-br.open('https://college.astanait.edu.kz/login/index.php')
-form = br.get_form()
-form['username']='karakuzov'
-form['password']='Kuanis2006$'
-br.submit_form(form)
-src=str(br.parsed())
 
+
+import psycopg2
+
+conn = psycopg2.connect(host = 'localhost',
+                       database = 'ABOB',
+                       port = '5433',
+                       user = 'postgres',
+                       password = '1234' )
+cur = conn.cursor()
+
+cur.execute("SELECT username FROM ABOB32")
+usernames = [r[0] for r in cur.fetchall()]
+Found = False 
+while not Found:
+    username = input('Login:')
+    if username in usernames:
+        print('Welcome')
+        Found=True
+    else:
+        print('Enter valid login please')
+
+conn.commit()
+cur.close()
+conn.close()
+
+# import turtle
+
+# t = turtle.Turtle()
+
+# # рисуем лицо
+# t.circle(100)
+
+# # рисуем левый глаз
+# t.penup()
+# t.goto(-35, 120)
+# t.pendown()
+# t.dot(25)
+
+# # рисуем правый глаз
+# t.penup()
+# t.goto(35, 120)
+# t.pendown()
+# t.dot(25)
+
+# # улыбка
+# t.penup()
+# t.goto(-50, 60)
+# t.pendown()
+# t.setheading(-60)
+# t.circle(50, 120)
+
+# turtle.done()
